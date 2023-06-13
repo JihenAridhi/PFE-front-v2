44e718a202d28d46d5a8adec35f9dd3f7ddf6b6d
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-13 10:45:39 +0100

    Update footer.component.ts

--- src/app/components/Browse/footer/footer.component.ts
+++ src/app/components/Browse/footer/footer.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LanguageService} from "../../../services/language.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
-  content: any
-  constructor(private ls: LanguageService) {ls.getLanguage().subscribe(data => this.content=data)}
+export class FooterComponent implements OnInit{
+
+  infos!: any[];
+  content: any;
+  ngOnInit() {
+    this.fetchInfoData();
+  }
+
+  constructor(private ls: LanguageService, private http: HttpClient) {ls.getLanguage().subscribe(data => this.content=data)}
+  private fetchInfoData() {
+    this.http.get<any[]>('/assets/BrowseInfo/footer.json').subscribe(data => {
+      this.infos = data;
+    });
+  }
 
 }
