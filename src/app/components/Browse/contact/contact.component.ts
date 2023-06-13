28d48de41d79c986e673d4c3847ab2b15d6f5b65
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-13 10:44:42 +0100

    Update contact.component.ts

--- src/app/components/Browse/contact/contact.component.ts
+++ src/app/components/Browse/contact/contact.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {FeedbackService} from "../../../services/feedback.service";
 import {LanguageService} from "../../../services/language.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  infos!: any[];
   content: any;
+  ngOnInit() {
+    this.fetchInfoData();
+  }
 
-  constructor(private fs: FeedbackService, private ls: LanguageService) {this.ls.getLanguage().subscribe(data => this.content = data)}
+  constructor(private fs: FeedbackService, private ls: LanguageService, private http: HttpClient) {this.ls.getLanguage().subscribe(data => this.content = data)}
   add(contactF: NgForm) {
     this.fs.add(contactF.value)
+  }
+  private fetchInfoData() {
+    this.http.get<any[]>('/assets/BrowseInfo/footer.json').subscribe(data => {
+      this.infos = data;
+    });
   }
 }
