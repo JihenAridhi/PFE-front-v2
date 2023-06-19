30df519c7154dc6b5e08ed809f5cb01ad6927ad9
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-19 12:09:29 +0100

    Update view-project.component.ts

--- src/app/components/Projects/view-project/view-project.component.ts
+++ src/app/components/Projects/view-project/view-project.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Project} from "../../../entities/Project";
+import {NewsService} from "../../../services/news.service";
+import {ActivatedRoute} from "@angular/router";
+import {ProjectService} from "../../../services/project.service";
 
 @Component({
   selector: 'app-view-project',
   templateUrl: './view-project.component.html',
   styleUrls: ['./view-project.component.css']
 })
-export class ViewProjectComponent {
-  project: any;
+export class ViewProjectComponent implements OnInit{
+  project: Project = new Project();
+  constructor(private ps: ProjectService, private route: ActivatedRoute) {}
 
+  async ngOnInit()
+  {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+    await this.ps.get(id).then(data => this.project=data!)
+    //await this.ns.getPhoto(this.news.id).then(data => this.url=data!)
+  }
 }
