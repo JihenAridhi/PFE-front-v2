3f38b2939429738ce002dfad7bd46a9b8af1a69a
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-19 12:08:13 +0100

    Update save-project.component.ts

--- src/app/components/Projects/save-project/save-project.component.ts
+++ src/app/components/Projects/save-project/save-project.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Project} from "../../../entities/Project";
 import {LanguageService} from "../../../services/language.service";
 import {NgForm} from "@angular/forms";
+import {ProjectService} from "../../../services/project.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-save-project',
@@ -12,8 +14,9 @@ export class SaveProjectComponent {
   project: Project = new Project();
   date: any;
   content: any;
+  private selectedFiles: any;
 
-  constructor(private ls: LanguageService) {ls.getLanguage().subscribe(data => this.content=data)}
+  constructor(private ls: LanguageService, private route: ActivatedRoute, private pr: ProjectService) {ls.getLanguage().subscribe(data => this.content=data)}
 
   async onFileSelected(files: any) {
     const file: File = files[0];
@@ -22,7 +25,18 @@ export class SaveProjectComponent {
     //await this.ps.setPhoto(formData).then()
   }
 
-  addproject(addF: NgForm) {
+  addproject(form: NgForm)
+  {
+    let project = form.value
+    project.date = new Date()
+    project.id=this.project.id
+    this.pr.save(project, this.selectedFiles)
+  }
 
+  ngOnInit(): void
+  {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+    if (id)
+      this.pr.get(id).then(data => this.project=data!)
   }
 }
